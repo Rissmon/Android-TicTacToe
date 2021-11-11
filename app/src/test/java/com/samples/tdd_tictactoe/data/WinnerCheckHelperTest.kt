35120df4de94bf26ba9c1e02ff0f4857427f3446
Rissmon <rissmon@gmail.com>
2021-11-11 17:27:47 +0530

    [Test] checkForWinner() will returns XPlayer if SecondRow is Selected.

--- app/src/test/java/com/samples/tdd_tictactoe/data/WinnerCheckHelperTest.kt
+++ app/src/test/java/com/samples/tdd_tictactoe/data/WinnerCheckHelperTest.kt
@@ -47,6 +47,20 @@ class WinnerCheckHelperTest {
         }
 
     @Test
+    fun checkForWinner_returnsXPlayerIfSecondRowHasXSelected() =
+        runBlockingTest {
+            val board = Board(cells = cleanBoardCells.toMutableList().apply {
+                set(3, Cell(0, 1, XSelected))
+                set(1, Cell(1, 0, OSelected))
+                set(4, Cell(1, 1, XSelected))
+                set(7, Cell(1, 2, OSelected))
+                set(5, Cell(2, 1, XSelected))
+            })
+
+            assertEquals(XPlayer, winnerCheckHelper.checkForWinner(board, Cell(2, 1, XSelected)))
+        }
+
+    @Test
     fun checkForWinner_returnsOPlayerIfRightDiagonalHasOSelected() =
         runBlockingTest {
             val board = Board(cells = cleanBoardCells.toMutableList().apply {
