4babdf1e156b9ce96567b0b2d49bf87eca45f4c1
Rissmon <rissmon@gmail.com>
2021-11-11 15:15:41 +0530

    [Test] Board isCompleted() fun will return false if all of the cell have clear state.

--- app/src/test/java/com/samples/tdd_tictactoe/model/BoardTest.kt
+++ app/src/test/java/com/samples/tdd_tictactoe/model/BoardTest.kt
@@ -7,6 +7,24 @@ import org.junit.Test
 class BoardTest {
 
     @Test
+    fun isCompleted_returnsFalseIfAllOfTheCellsHaveClearState() {
+        val board = Board(
+            listOf(
+                Cell(0, 0, Clear),
+                Cell(0, 1, Clear),
+                Cell(0, 2, Clear),
+                Cell(1, 0, Clear),
+                Cell(1, 1, Clear),
+                Cell(1, 2, Clear),
+                Cell(2, 0, Clear),
+                Cell(2, 1, Clear),
+                Cell(2, 2, Clear)
+            )
+        )
+        assertFalse(board.isCompleted())
+    }
+
+    @Test
     fun isCompleted_returnsFalseIfAnyOneOfTheCellHaveClearState() {
         val board = Board(
             listOf(
