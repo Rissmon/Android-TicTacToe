8bf5bd67c10e1cbe9eae31a8a6212a6e7d7b3d6d
Rissmon <rissmon@gmail.com>
2021-11-11 14:13:12 +0530

    [Test] Validate the navigation to GameBoardFragment if all the input fields are valid.

--- app/src/androidTest/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryFragmentTest.kt
+++ app/src/androidTest/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryFragmentTest.kt
@@ -1,11 +1,15 @@
 package com.samples.tdd_tictactoe.view.entry
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.samples.tdd_tictactoe.R
 import com.samples.tdd_tictactoe.utils.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 
@@ -32,7 +36,6 @@ class PlayerEntryFragmentTest {
         validateSnackbarMessage(R.string.enter_all_fields)
     }
 
-
     @Test
     fun shouldShowErrorMessage_WhenEitherOfTheInputFieldsAreEmpty() {
         //Validate Player 1
@@ -46,6 +49,21 @@ class PlayerEntryFragmentTest {
         onView(withId(R.id.editTextPlayer2)).perform(ViewActions.typeText(PLAYER2))
         onView(withId(R.id.buttonPlay)).perform(ViewActions.click())
         validateSnackbarMessage(R.string.enter_all_fields)
+    }
+
+    @Test
+    fun shouldGameScreen_WhenAllInputFieldsAreValid() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        val scenario = launchFragment()
+        scenario.onFragment {
+            navController.setGraph(R.navigation.nav_graph)
+            navController.setCurrentDestination(R.id.entryFragment)
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+        onView(withId(R.id.editTextPlayer1)).perform(ViewActions.typeText(PLAYER1))
+        onView(withId(R.id.editTextPlayer2)).perform(ViewActions.typeText(PLAYER2))
+        onView(withId(R.id.buttonPlay)).perform(ViewActions.click())
+        assertEquals(navController.currentDestination?.id, R.id.gameFragment)
     }
 
     private fun launchFragment() =
--- app/src/main/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryFragment.kt
+++ app/src/main/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryFragment.kt
@@ -3,10 +3,13 @@ package com.samples.tdd_tictactoe.view.entry
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.samples.tdd_tictactoe.R
 import com.samples.tdd_tictactoe.base.BaseFragment
 import com.samples.tdd_tictactoe.common.extension.showSnackbar
+import com.samples.tdd_tictactoe.common.nonNull
 import com.samples.tdd_tictactoe.databinding.FragmentPlayerEntryBinding
+import com.samples.tdd_tictactoe.model.PlayerData
 
 class PlayerEntryFragment :
     BaseFragment<FragmentPlayerEntryBinding>(R.layout.fragment_player_entry) {
@@ -18,6 +21,18 @@ class PlayerEntryFragment :
         getDataBinding().buttonPlay.setOnClickListener {
             if (viewModel.isPlayerFieldNullOrEmpty)
                 getDataBinding().root.showSnackbar(R.string.enter_all_fields)
+            else navigate()
+
         }
+    }
+
+    private fun navigate() {
+        val actions = PlayerEntryFragmentDirections.actionEntryFragmentToGameFragment(
+            PlayerData(
+                nonNull(viewModel.playerX.value),
+                nonNull(viewModel.playerO.value)
+            )
+        )
+        findNavController().navigate(actions)
     }
 }
