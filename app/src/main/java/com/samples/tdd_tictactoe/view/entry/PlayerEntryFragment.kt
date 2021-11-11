48cc228b85d1ff3fff2679e40ba287a61e54c73c
Rissmon <rissmon@gmail.com>
2021-11-11 11:26:41 +0530

    [Data Binding] Assign PlayerEntryViewModel directly to the PlayerEntryFragment layout file.

--- app/src/main/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryFragment.kt
+++ app/src/main/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryFragment.kt
@@ -1,7 +1,18 @@
 package com.samples.tdd_tictactoe.view.entry
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.samples.tdd_tictactoe.R
 import com.samples.tdd_tictactoe.base.BaseFragment
 import com.samples.tdd_tictactoe.databinding.FragmentPlayerEntryBinding
 
-class PlayerEntryFragment : BaseFragment<FragmentPlayerEntryBinding>(R.layout.fragment_player_entry)
+class PlayerEntryFragment :
+    BaseFragment<FragmentPlayerEntryBinding>(R.layout.fragment_player_entry) {
+    private val viewModel: PlayerEntryViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getDataBinding().playerEntryViewModel = viewModel
+    }
+}
--- app/src/main/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryViewModel.kt
+++ app/src/main/java/com/samples/tdd_tictactoe/view/entry/PlayerEntryViewModel.kt
@@ -1,7 +1,13 @@
 package com.samples.tdd_tictactoe.view.entry
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.samples.tdd_tictactoe.common.extension.empty
 
-class PlayerEntryViewModel : ViewModel()
+class PlayerEntryViewModel : ViewModel() {
+    //Value need to mutable since there is databinding
+    val playerX: MutableLiveData<String> = MutableLiveData(String.empty())
+    val playerO: MutableLiveData<String> = MutableLiveData(String.empty())
+}
 
 
