b4247b158eace04bdad74e9d38ce8893c05a476e
Rissmon <rissmon@gmail.com>
2021-11-12 00:27:54 +0530

    Hilt missing configuration added in activity test.

--- app/src/androidTest/java/com/samples/tdd_tictactoe/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/samples/tdd_tictactoe/view/MainActivityTest.kt
@@ -6,15 +6,22 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.samples.tdd_tictactoe.R
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
+@HiltAndroidTest
 class MainActivityTest {
     private val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
     private lateinit var activityScenario: ActivityScenario<MainActivity>
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Before
     fun init() {
